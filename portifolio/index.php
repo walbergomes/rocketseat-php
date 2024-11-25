1ef771452e116d9bbc9ba3cd71db6f84e01504a3
Walber <walberg.s48@gmail.com>
2024-11-25 17:11:04 -0300

    finalizando arrays em php

--- portifolio/index.php
+++ portifolio/index.php
@@ -1,98 +1,76 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Meu Portfólio</title>
 </head>
+
 <body>
-  <?php 
-    $name = "Walber";
-    $saudacao = "Oi, "; 
-    $titulo = $saudacao . "Esse é o portifólio do " .  $name;
-    $subtitulo = "Seja bem vindo ao meu portifólio";
-    $ano = 2024;
+  <?php
+  $name = "Walber";
+  $saudacao = "Oi, ";
+  $titulo = $saudacao . "Esse é o portifólio do " .  $name;
+  $subtitulo = "Seja bem vindo ao meu portifólio";
+  $ano = 2024;
 
-    $projeto = "Meu portifólio";
-    $finalizado = true;
-    $dataDoProjeto = "2024-11-25";
-    $descricao = "Meu primeiro portifólio em PHP e HTML";
+  $projeto = "Meu portifólio";
+  $finalizado = true;
+  $dataDoProjeto = "2024-11-25";
+  $descricao = "Meu primeiro portifólio em PHP e HTML";
 
-    $projetos = [
-      [
-        "titulo" => "Meu portifólio",
-        "finalizado" => false,
-        "data" => "2024-11-25",
-        "descricao" => "Meu primeiro portifólio. Escrito em PHP e HTML."
-      ],
-      // "Lista de Tarefas",
-      // "Controle de leitura de livros",
-      // "mais um projeto",
-    ];
+  $projetos = [
+    [
+      "titulo" => "Meu portifólio",
+      "finalizado" => true,
+      "data" => "2024-11-25",
+      "descricao" => "Meu primeiro portifólio. Escrito em PHP e HTML."
+    ],
+    [
+      "titulo" => "Lista de tarefas",
+      "finalizado" => false,
+      "data" => "2024-05-25",
+      "descricao" => "Lista de tarefas Escrito em PHP e HTML."
+    ],
+  ];
   ?>
 
   <h1><?php echo $titulo; ?></h1>
 
   <!-- Pode ser escrito assim também - é a mesma coisa que usar echo, só é mais curto -->
   <!-- Nessa versão, o ";" não é obrigatório -->
-  <p><?=$subtitulo?> </p>
-  <p><?=$ano?> </p>
+  <p><?= $subtitulo ?> </p>
+  <p><?= $ano ?> </p>
 
 
   <hr>
 
   <ul>
-    <?php foreach($projetos as $projeto) {
-      echo "<li>{$projeto['titulo']}</li>";
-      echo "<li>{$projeto['data']}</li>";
-      echo "<li>{$projeto['descricao']}</li>";
-    }
+    <?php foreach ($projetos as $projeto): ?>
+      <div
+        <?php if (! ((2024 - $ano) > 2)): ?>
+        style="background-color: burlywood;"
+        <?php endif; ?>>
+        <h2><?=$projeto['titulo']?></h2>
+        <p><?= $projeto['descricao'] ?></p>
 
+        <div>
+          <div><?=$projeto['data'] ?></div>
+          <div>
+            Projeto:
 
-    ?>
-  </ul>
+            <?php if ($projeto['finalizado']): ?>
+              <span style="color: green;"> ✅ finalizado </span>
+            <?php else: ?>
+              <span style="color: red;"> 🚫 não finalizado </span>
+            <?php endif ?>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  <!-- <div 
-    <?php if( ! ((2024 - $ano) > 2) ): ?>
-      style="background-color: burlywood;"
-    <?php endif; ?>
-
-  >
-    <h2><?=$projeto?></h2>
-    <p><?=$descricao?></p>
-
-    <div>
-      <div><?=$dataDoProjeto?></div>
-      <div> 
-        Projeto: 
-
-        <?php  if($finalizado): ?> 
-          <span style="color: green;"> ✅ finalizado </span>
-        <?php else: ?> 
-          <span style="color: red;"> 🚫 não finalizado </span>
-        <?php endif ?>
-
+          </div>
+        </div>
       </div>
-    </div>
-  </div> -->
+    <?php endforeach; ?>
+  </ul>
 </body>
-</html> 
+
+</html>

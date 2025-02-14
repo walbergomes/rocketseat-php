478966a560763df28fef5cb10c2d0b7413d7d42b
Walber <walberg.s48@gmail.com>
2025-02-14 16:26:59 -0300

    criando arquivo reserva e separando em componentes

---
+++ introducao/desafio-portfolio-dev/components/header.php
@@ -0,0 +1,29 @@
+<header class="flex flex-col items-center pt-40">
+  <div class="size-fit p-1 rounded-full border-[3px] border-red-400">
+    <img src="assets/avatar.png" alt="" class="" />
+  </div>
+
+  <p class="text-gray-200 mt-10 text-xl">
+    Hello World! Meu nome é
+    <span class="text-red-300">Martina Santos</span> e sou
+  </p>
+  <h1 class="text-gray-200 text-6xl font-bold mt-4">Desenvolvedora PHP</h1>
+  <p class="max-w-[680px] text-gray-400 text-sm text-center mt-8">
+    Transformo necessidades em aplicações reais, evolventes e funcionais.
+    Desenvolvo sistemas através da minha paixão pela tecnologia,
+    contribuindo com soluções inovadoras e eficazes para desafios complexos.
+  </p>
+
+  <div class="mt-15">
+    <span
+      class="bg-emerald-500 px-3 py-1 border-[2px] rounded-3xl text-base text-bg-900">Github</span>
+    <span
+      class="bg-fuchsia-500 px-3 py-1 border-[2px] rounded-3xl text-base text-bg-900">PHP</span>
+    <span
+      class="bg-rose-500 px-3 py-1 border-[2px] rounded-3xl text-base text-bg-900">CSS</span>
+    <span
+      class="bg-sky-500 px-3 py-1 border-[2px] rounded-3xl text-base text-bg-900">HTML</span>
+    <span
+      class="bg-yellow-500 px-3 py-1 border-[2px] rounded-3xl text-base text-bg-900">JavaScript</span>
+  </div>
+</header>
---
+++ introducao/desafio-portfolio-dev/components/projects.php
@@ -0,0 +1,151 @@
+<div class="max-w-screen-lg w-full mt-15 grid grid-cols-2 gap-3">
+  <div class="bg-stone-800 h-45 rounded-xl p-3 flex gap-3">
+    <div
+      class="w-2/5 rounded-xl bg-[url('assets/thumbs/project-1.png')] bg-cover bg-center"></div>
+    <div class="w-3/5 flex flex-col justify-between">
+      <header>
+        <h3 class="text-gray-200 font-bold text-base">Travelgram</h3>
+        <p class="text-sm mt-3 text-neutral-500">
+          Rede social onde as pessoas mostram os registros de suas viagens
+          pelo mundo
+        </p>
+      </header>
+
+      <footer>
+        <span
+          class="bg-rose-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">PHP</span>
+        <span
+          class="bg-purple-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">CSS</span>
+        <span
+          class="bg-indigo-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">HTML</span>
+        <span
+          class="bg-teal-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">JavaScript</span>
+      </footer>
+    </div>
+  </div>
+
+  <div class="bg-stone-800 h-45 rounded-xl p-3 flex gap-3">
+    <div
+      class="w-2/5 rounded-xl bg-[url('assets/thumbs/project-2.png')] bg-cover bg-center"></div>
+    <div class="w-3/5 flex flex-col justify-between">
+      <header>
+        <h3 class="text-gray-200 font-bold text-base">
+          Página de receita
+        </h3>
+        <p class="text-sm mt-3 text-neutral-500">
+          Página com o passo a passo de uma receita para cupcakes
+        </p>
+      </header>
+
+      <footer>
+        <span
+          class="bg-rose-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">PHP</span>
+        <span
+          class="bg-purple-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">CSS</span>
+        <span
+          class="bg-indigo-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">HTML</span>
+        <span
+          class="bg-teal-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">JavaScript</span>
+      </footer>
+    </div>
+  </div>
+
+  <div class="bg-stone-800 h-45 rounded-xl p-3 flex gap-3">
+    <div
+      class="w-2/5 rounded-xl bg-[url('assets/thumbs/project-3.png')] bg-cover bg-center"></div>
+    <div class="w-3/5 flex flex-col justify-between">
+      <header>
+        <h3 class="text-gray-200 font-bold text-base">Tech News</h3>
+        <p class="text-sm mt-3 text-neutral-500">
+          Homepage de um portal de notícias sobre a área de tecnologia
+        </p>
+      </header>
+
+      <footer>
+        <span
+          class="bg-rose-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">PHP</span>
+        <span
+          class="bg-purple-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">CSS</span>
+        <span
+          class="bg-indigo-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">HTML</span>
+        <span
+          class="bg-teal-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">JavaScript</span>
+      </footer>
+    </div>
+  </div>
+
+  <div class="bg-stone-800 h-45 rounded-xl p-3 flex gap-3">
+    <div
+      class="w-2/5 rounded-xl bg-[url('assets/thumbs/project-4.png')] bg-cover bg-center"></div>
+    <div class="w-3/5 flex flex-col justify-between">
+      <header>
+        <h3 class="text-gray-200 font-bold text-base">Refund</h3>
+        <p class="text-sm mt-3 text-neutral-500">
+          Um sistema para pedido e acompanhamento de reembolso
+        </p>
+      </header>
+
+      <footer>
+        <span
+          class="bg-rose-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">PHP</span>
+        <span
+          class="bg-purple-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">CSS</span>
+        <span
+          class="bg-indigo-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">HTML</span>
+        <span
+          class="bg-teal-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">JavaScript</span>
+      </footer>
+    </div>
+  </div>
+
+  <div class="bg-stone-800 h-45 rounded-xl p-3 flex gap-3">
+    <div
+      class="w-2/5 rounded-xl bg-[url('assets/thumbs/project-5.png')] bg-cover bg-center"></div>
+    <div class="w-3/5 flex flex-col justify-between">
+      <header>
+        <h3 class="text-gray-200 font-bold text-base">
+          Página de turismo
+        </h3>
+        <p class="text-sm mt-3 text-neutral-500">
+          Página com as principais informações para quem quer viajar para
+          Busan
+        </p>
+      </header>
+
+      <footer>
+        <span
+          class="bg-rose-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">PHP</span>
+        <span
+          class="bg-purple-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">CSS</span>
+        <span
+          class="bg-indigo-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">HTML</span>
+        <span
+          class="bg-teal-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">JavaScript</span>
+      </footer>
+    </div>
+  </div>
+
+  <div class="bg-stone-800 h-45 rounded-xl p-3 flex gap-3">
+    <div
+      class="w-2/5 rounded-xl bg-[url('assets/thumbs/project-6.png')] bg-cover bg-center"></div>
+    <div class="w-3/5 flex flex-col justify-between">
+      <header>
+        <h3 class="text-gray-200 font-bold text-base">Zingen</h3>
+        <p class="text-sm mt-3 text-neutral-500">
+          Landing Page completa e responsiva de um aplicativo de Karaokê
+        </p>
+      </header>
+
+      <footer>
+        <span
+          class="bg-rose-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">PHP</span>
+        <span
+          class="bg-purple-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">CSS</span>
+        <span
+          class="bg-indigo-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">HTML</span>
+        <span
+          class="bg-teal-500 py-1 px-2 rounded text-sm text-stone-900 font-semibold">JavaScript</span>
+      </footer>
+    </div>
+  </div>
+</div>

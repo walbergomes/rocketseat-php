beee5c6a9003fbf66cead69e49c619c69eb6788e
Walber <walberg.s48@gmail.com>
2025-02-14 18:05:52 -0300

    dinamizando cards de contato

--- introducao/desafio-portfolio-dev/components/contact-cards.php
+++ introducao/desafio-portfolio-dev/components/contact-cards.php
@@ -1,42 +1,36 @@
+<?php
+$links = [
+  [
+    "img" => "assets/social/linkedin.svg",
+    "title" => "Linkedin"
+  ],
+  [
+    "img" => "assets/social/instagram.svg",
+    "title" => "Instagram"
+  ],
+  [
+    "img" => "assets/social/github.svg",
+    "title" => "GitHub"
+  ],
+  [
+    "img" => "assets/social/email.svg",
+    "title" => "E-mail"
+  ],
+]
+?>
+
 <div class="cards flex flex-col gap-5">
-  <div class="card bg-stone-800 w-[400px] p-5 rounded-xl">
-    <a href="#" class="flex justify-between">
-      <div class="flex gap-3 text-gray-400 font-medium">
-        <img src="assets/social/linkedin.svg" alt="" />
-        Linkedin
-      </div>
-      <img src="assets/arrow.svg" alt="" />
-    </a>
-  </div>
+  <?php foreach ($links as $link): ?>
 
-  <div class="card bg-stone-800 w-[400px] p-5 rounded-xl">
-    <a href="#" class="flex justify-between">
-      <div class="flex gap-3 text-gray-400 font-medium">
-        <img src="assets/social/instagram.svg" alt="" />
-        Instagram
-      </div>
-      <img src="assets/arrow.svg" alt="" />
-    </a>
-  </div>
+    <div class="card bg-stone-800 w-[400px] p-5 rounded-xl">
+      <a href="#" class="flex justify-between">
+        <div class="flex gap-3 text-gray-400 font-medium">
+          <img src="<?=$link["img"]?>" alt="" />
+          <?=$link["title"]?>
+        </div>
+        <img src="assets/arrow.svg" alt="" />
+      </a>
+    </div>
 
-  <div class="card bg-stone-800 w-[400px] p-5 rounded-xl">
-    <a href="#" class="flex justify-between">
-      <div class="flex gap-3 text-gray-400 font-medium">
-        <img src="assets/social/github.svg" alt="" />
-        GitHub
-      </div>
-      <img src="assets/arrow.svg" alt="" />
-    </a>
-  </div>
-
-  <div class="card bg-stone-800 w-[400px] p-5 rounded-xl">
-    <a href="#" class="flex justify-between">
-      <div class="flex gap-3 text-gray-400 font-medium">
-        <img src="assets/social/email.svg" alt="" />
-        E-mail
-      </div>
-      <img src="assets/arrow.svg" alt="" />
-    </a>
-  </div>
-
+  <?php endforeach; ?>
 </div>

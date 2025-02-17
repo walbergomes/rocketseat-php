0c889192375875a1e5fbe0d79b71fe767d8ebc2c
Walber <walberg.s48@gmail.com>
2025-02-17 16:59:50 -0300

    criando e estilizando o form

--- primeiros-passos/bookwise/index.php
+++ primeiros-passos/bookwise/index.php
@@ -8,7 +8,7 @@
 </head>
 <body class="bg-stone-950 text-stone-200">
   <header class="bg-stone-900">
-    <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+    <nav class="mx-auto max-w-screen-lg flex justify-between py-4">
       <div class="font-bold text-xl tracking-wide">Book Wise</div>
       <ul class="flex space-x-4 font-bold">
         <li><a href="/" class="text-lime-500">Explorar</a></li>
@@ -20,8 +20,19 @@
     </nav>
   </header>
 
-  <main>
-    Lista de livros
+  <main class="mx-auto max-w-screen-lg space-y-6">
+    <form class="w-full flex space-x-2 mt-6">
+      <input 
+        type="text" 
+        class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1"
+        placeholder="Pesquisar..."
+        name="pesquisar"
+      >
+      <button type="submit">🔎</button>
+    </form>
+    <div>
+      Lista Final
+    </div>
   </main>
 </body>
 </html>

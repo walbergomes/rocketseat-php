283c2b9589722e3f32509dcea44dc48f692b523b
Walber <walberg.s48@gmail.com>
2025-02-17 17:41:08 -0300

    criando a lista de livros

--- primeiros-passos/bookwise/index.php
+++ primeiros-passos/bookwise/index.php
@@ -31,14 +31,86 @@
         name="pesquisar">
       <button type="submit">🔎</button>
     </form>
-
-    <section class="space-y-4">
+    
+    <!-- lista de livros -->
+    <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <!-- Livro -->
-      <div class="w-1/3 p-2 rounded border-stone-800 border-2 bg-stone-900">
+       
+      <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
         <div class="flex">
           <div class="w-1/3">Imagem</div>
           <div>
-            <div class="font-semibold">Titulo</div>
+            <a href="/livro.php" class="font-semibold hover:underline">Titulo</a>
+            <div class="text-xs italic">Autor</div>
+            <div class="text-xs italic">⭐⭐⭐⭐⭐ (3 Avaliações)</div>
+          </div>
+        </div>
+        <div class="text-sm">
+          Descrição
+        </div>
+      </div>
+
+      <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+        <div class="flex">
+          <div class="w-1/3">Imagem</div>
+          <div>
+            <a href="/livro.php" class="font-semibold hover:underline">Titulo</a>
+            <div class="text-xs italic">Autor</div>
+            <div class="text-xs italic">⭐⭐⭐⭐⭐ (3 Avaliações)</div>
+          </div>
+        </div>
+        <div class="text-sm">
+          Descrição
+        </div>
+      </div>
+
+      <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+        <div class="flex">
+          <div class="w-1/3">Imagem</div>
+          <div>
+            <a href="/livro.php" class="font-semibold hover:underline">Titulo</a>
+            <div class="text-xs italic">Autor</div>
+            <div class="text-xs italic">⭐⭐⭐⭐⭐ (3 Avaliações)</div>
+          </div>
+        </div>
+        <div class="text-sm">
+          Descrição
+        </div>
+      </div>
+
+      <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+        <div class="flex">
+          <div class="w-1/3">Imagem</div>
+          <div>
+            <a href="/livro.php" class="font-semibold hover:underline">Titulo</a>
+            <div class="text-xs italic">Autor</div>
+            <div class="text-xs italic">⭐⭐⭐⭐⭐ (3 Avaliações)</div>
+          </div>
+        </div>
+        <div class="text-sm">
+          Descrição
+        </div>
+      </div>
+
+      <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+        <div class="flex">
+          <div class="w-1/3">Imagem</div>
+          <div>
+            <a href="/livro.php" class="font-semibold hover:underline">Titulo</a>
+            <div class="text-xs italic">Autor</div>
+            <div class="text-xs italic">⭐⭐⭐⭐⭐ (3 Avaliações)</div>
+          </div>
+        </div>
+        <div class="text-sm">
+          Descrição
+        </div>
+      </div>
+
+      <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+        <div class="flex">
+          <div class="w-1/3">Imagem</div>
+          <div>
+            <a href="/livro.php" class="font-semibold hover:underline">Titulo</a>
             <div class="text-xs italic">Autor</div>
             <div class="text-xs italic">⭐⭐⭐⭐⭐ (3 Avaliações)</div>
           </div>
---
+++ primeiros-passos/bookwise/livro.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BookWise</title>
+  <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+</head>
+
+<body class="bg-stone-950 text-stone-200">
+  <header class="bg-stone-900">
+    <nav class="mx-auto max-w-screen-lg flex justify-between py-4">
+      <div class="font-bold text-xl tracking-wide">Book Wise</div>
+      <ul class="flex space-x-4 font-bold">
+        <li><a href="/" class="text-lime-500">Explorar</a></li>
+        <li><a href="/meus-livros.php" class="hover:underline">Meus Livros</a></li>
+      </ul>
+      <ul>
+        <li><a href="/login" class="hover:underline">Fazer Login</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main class="mx-auto max-w-screen-lg space-y-6">
+    // muda aqui
+  </main>
+</body>
+
+</html>

5adeeba5ea9bd12784e88f83d9c237360c419191
Walber <walberg.s48@gmail.com>
2025-03-07 18:42:01 -0300

    N2 Banco de Dados - acessando os dados do banco com php

--- nivel-02/bookwise/dados.php
+++ nivel-02/bookwise/dados.php
@@ -1,15 +1,9 @@
 <?php
 
-$livros = [
-  ['id' => 1, 'titulo' => '1984', 'autor' => 'George Orwell', 'descricao' => 'Um romance distópico sobre um governo totalitário.'],
-  ['id' => 2, 'titulo' => 'Dom Casmurro', 'autor' => 'Machado de Assis', 'descricao' => 'Um dos maiores clássicos da literatura brasileira.'],
-  ['id' => 3, 'titulo' => 'A Revolução dos Bichos', 'autor' => 'George Orwell', 'descricao' => 'Uma fábula política sobre a corrupção do poder.'],
-  ['id' => 4, 'titulo' => 'O Pequeno Príncipe', 'autor' => 'Antoine de Saint-Exupéry', 'descricao' => 'Uma história filosófica sobre amizade e amor.'],
-  ['id' => 5, 'titulo' => 'O Hobbit', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'Uma aventura fantástica na Terra Média.'],
-  ['id' => 6, 'titulo' => 'Crime e Castigo', 'autor' => 'Fiódor Dostoiévski', 'descricao' => 'Um estudo psicológico sobre culpa e redenção.'],
-  ['id' => 7, 'titulo' => 'Orgulho e Preconceito', 'autor' => 'Jane Austen', 'descricao' => 'Um romance clássico sobre amor e sociedade.'],
-  ['id' => 8, 'titulo' => 'O Senhor dos Anéis', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'Uma das maiores obras da fantasia épica.'],
-  ['id' => 9, 'titulo' => 'Cem Anos de Solidão', 'autor' => 'Gabriel García Márquez', 'descricao' => 'Um clássico do realismo mágico.'],
-];
+$db = new PDO('sqlite:database.sqlite');
+$query = $db->query('select * from livros');
+
+$livros = $query->fetchAll();
+
 
 ?>

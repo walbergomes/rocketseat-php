24143e8c96bc5b132ef69b91c495280db6980fd3
Walber <walberg.s48@gmail.com>
2025-03-11 18:19:03 -0300

    N2 Banco de Dados - validacoes

--- nivel-02/bookwise/index.php
+++ nivel-02/bookwise/index.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 require "functions.php";
 require "models/Livro.php";
 $config = require "config.php";
--- nivel-02/bookwise/views/login.view.php
+++ nivel-02/bookwise/views/login.view.php
@@ -32,26 +32,38 @@
 
   </div>
 
-  
+
   <div class="border border-stone-700 rounded">
     <h1 class="border-b border-stone-700 text-stone-400 font-bold px-4 py-2">Registro</h1>
 
     <form class="p-4 space-y-4" method="POST" action="/registrar">
-      <?php if (strlen($mensagem) > 0): ?>
+      <?php if (isset($mensagem) && strlen($mensagem)): ?>
 
-        <div class="border-green-800 bg-green-900 text-green-400 px-4 py-1 rounded-md border-2">
-          <?=$mensagem?>
+        <div class="border-green-800 bg-green-900 text-green-400 px-4 py-1 rounded-md border-2 text-sm font-bold">
+          <?= $mensagem ?>
         </div>
 
       <?php endif; ?>
-        
+
+      <?php if (isset($_SESSION['validacoes']) && sizeof($_SESSION['validacoes'])): ?>
+
+        <div class="border-red-800 bg-red-900 text-red-400 px-4 py-1 rounded-md border-2 text-sm font-bold">
+          <ul>
+            <li>Deu ruim</li>
+            <?php foreach ($_SESSION['validacoes'] as $validacao): ?>
+              <li><?= $validacao ?> </li>
+            <?php endforeach; ?>
+          </ul>
+        </div>
+
+      <?php endif; ?>
+
       <div class="flex flex-col">
         <label class="text-stone-400 mb-1">Nome</label>
         <input
           type="text"
           class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1"
-          name="nome"
-          require>
+          name="nome">
       </div>
 
       <div class="flex flex-col">
@@ -59,8 +71,7 @@
         <input
           type="email"
           class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1"
-          name="email"
-          require>
+          name="email">
       </div>
 
       <div class="flex flex-col">
@@ -68,8 +79,7 @@
         <input
           type="email"
           class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1"
-          name="email_confirmacao"
-          require>
+          name="email_confirmacao">
       </div>
 
       <div class="flex flex-col">
@@ -77,8 +87,7 @@
         <input
           type="password"
           class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1"
-          name="senha"
-          require>
+          name="senha">
       </div>
 
       <button

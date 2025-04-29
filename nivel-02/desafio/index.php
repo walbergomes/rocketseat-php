5b3ddd0fb6ba8a062d906062f6d5413cf96b1586
Walber <walberg.s48@gmail.com>
2025-04-29 15:19:20 -0300

    Challange PHP - creating Details page (Film Section completed)

--- nivel-02/desafio/index.php
+++ nivel-02/desafio/index.php
@@ -72,7 +72,56 @@
   </header>
 
   <div class="max-w-7xl m-auto pb-[60px]">
+    <main class="w-full h-full mt-16 flex gap-12">
 
+      <!-- ### FILM IMAGE SIDE ### -->
+      <section class="w-fit">
+        <figure class="card w-[381px] h-[490px] bg-red-500 rounded-2xl overflow-hidden relative">
+          <img
+            src="assets/Image/Films/pobres-criaturas.png"
+            alt=""
+            class="w-full h-full object-cover">
+          <div class="shadow absolute bottom-0 left-0 w-full h-[100px] 
+          bg-gradient-to-b from-transparent to-gray-900/90"></div>
+        </figure>
+      </section>
+
+      <!-- ### FILM INFOS SIDE ### -->
+      <section class="w-2/3 flex flex-col">
+        <button class="text-base font-nunito text-gray-500 flex items-center gap-2 cursor-pointer w-fit hover:brightness-[.8] mt-[10px]">
+          <img src="assets/Icon/ArrowLeft-Regular.svg" alt="" class="w-[20px]">
+          Voltar
+        </button>
+
+        <header class="mt-5 flex flex-col gap-3">
+          <h2 class="font-rajdhani font-bold text-3xl text-gray-700">Pobres criaturas</h2>
+          <div id="info" class="font-rajdhani font-bold text-3xl">
+            <div>
+              <span class="font-nunito text-base text-gray-600 font-bold">Categoria:</span>
+              <span class="font-nunito text-base text-gray-600 font-normal">Drama</span>
+            </div>
+            <div>
+              <span class="font-nunito text-base text-gray-600 font-bold">Ano:</span>
+              <span class="font-nunito text-base text-gray-600 font-normal">2023</span>
+            </div>
+          </div>
+          <div id="stars" class="flex gap-2 items-center">
+            <img src="assets/Icon/Star-Fill-Purple.svg" alt="" class="w-[24px]">
+            <img src="assets/Icon/Star-Fill-Purple.svg" alt="" class="w-[24px]">
+            <img src="assets/Icon/Star-Fill-Purple.svg" alt="" class="w-[24px]">
+            <img src="assets/Icon/Star-Fill-Purple.svg" alt="" class="w-[24px]">
+            <img src="assets/Icon/Star-Regular-Purple.svg" alt="" class="w-[24px]">
+
+            <span class="ml-2 font-nunito text-2xl text-gray-700 font-bold">4</span>
+            <span class="font-nunito text-base text-gray-600">(5) Avaliações</span>
+          </div>
+        </header>
+
+        <p class="font-nunito text-base text-gray-600 mt-[80px]">
+          Do cineasta Yorgos Lanthimos e da produtora Emma Stone, vem o conto incrível e a evolução fantástica de Bella Baxter (Stone), uma jovem mulher trazida de volta à vida pelo brilhante e pouco ortodoxo cientista Dr. Godwin Baxter (Willem Dafoe). Sob a proteção de Baxter, Bella está ansiosa para aprender. Faminta pela mundanidade que lhe falta, Bella foge com Duncan Wedderburn (Mark Ruffalo), um advogado astuto e depravado, em uma aventura relâmpago pelos continentes. Livre dos preconceitos de sua época, Bella se torna firme em seu propósito de defender a igualdade e a libertação.
+        </p>
+      </section>
+    </main>
   </div>
 </body>
 

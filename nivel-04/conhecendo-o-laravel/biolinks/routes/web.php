ddc619d44f9dbcbd1bd06129b1e283d5ac06d70c
Walber <walberg.s48@gmail.com>
2025-07-09 18:54:51 -0300

    criando nosso controller e nossa rota de login

--- nivel-04/conhecendo-o-laravel/biolinks/routes/web.php
+++ nivel-04/conhecendo-o-laravel/biolinks/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [LoginController::class, 'index']);

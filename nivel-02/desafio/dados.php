b2a2909f3068916ba171242e3a97a2a68557a871
Walber <walberg.s48@gmail.com>
2025-08-15 21:10:53 -0300

    :card_file_box: create database films files

---
+++ nivel-02/desafio/dados.php
@@ -0,0 +1,19 @@
+<?php
+
+$films = [
+  ['id' => 1, 'title' => 'Pobres Criaturas', 'gender' => 'Drama', 'year' => 2023, 'img' => 'pobres-criaturas.png', 'stars' => '4,7', 'description' => 'Do cineasta Yorgos Lanthimos e da produtora Emma Stone, vem o conto incrível e a evolução fantástica de Bella Baxter (Stone), uma jovem mulher trazida de volta à vida pelo brilhante e pouco ortodoxo cientista Dr. Godwin Baxter (Willem Dafoe). Sob a proteção de Baxter, Bella está ansiosa para aprender. Faminta pela mundanidade que lhe falta, Bella foge com Duncan Wedderburn (Mark Ruffalo), um advogado astuto e depravado, em uma aventura relâmpago pelos continentes. Livre dos preconceitos de sua época, Bella se torna firme em seu propósito de defender a igualdade e a libertação.'],
+  
+  ['id' => 2, 'title' => 'Meu Malvado Favorito 4', 'gender' => 'Animação', 'year' => 2024, 'img' => 'meu-malvado.png', 'stars' => '4,7', 'description' => 'Gru agora enfrenta uma nova ameaça: Maxime Le Mal e sua namorada Valentina. Com sua família crescendo e novos desafios surgindo, ele precisa equilibrar sua vida de pai com suas antigas habilidades de vilão. Repleto de humor, ação e personagens cativantes.'],
+  
+  ['id' => 3, 'title' => 'Deadpool & Wolverine', 'gender' => 'Ação', 'year' => 2023, 'img' => 'deadpol-x-wolverine.png', 'stars' => '4,7', 'description' => 'Neste encontro explosivo, Deadpool e Wolverine se veem forçados a trabalhar juntos para enfrentar uma ameaça que pode destruir tudo. Repleto de ação brutal, humor ácido e a clássica quebra da quarta parede, o filme é uma celebração caótica dos dois heróis mais irreverentes da Marvel.'],
+  
+  ['id' => 4, 'title' => 'O Corvo', 'gender' => 'Fantasia', 'year' => 2024, 'img' => 'o-corvo.png', 'stars' => '4,7', 'description' => 'Após ser brutalmente assassinado junto de sua noiva, um homem retorna à vida com poderes sobrenaturais concedidos por um corvo místico. Em busca de vingança e redenção, ele enfrenta os responsáveis por sua tragédia em uma sombria jornada de justiça poética.'],
+  
+  ['id' => 5, 'title' => 'O Senhor dos Anéis: A Sociedade do Anel', 'gender' => 'Aventura', 'year' => 2001, 'img' => 'senhor-dos-aneis.png', 'stars' => '5', 'description' => 'Frodo Bolseiro herda um anel mágico com um poder devastador e embarca em uma missão épica para destruí-lo nas chamas da Montanha da Perdição. Acompanhado por uma sociedade diversa, enfrenta perigos, traições e descobertas em uma jornada que marcará a Terra-média para sempre.'],
+  
+  ['id' => 6, 'title' => 'Alien: Covenant', 'gender' => 'Horror', 'year' => 2017, 'img' => 'alien.png', 'stars' => '4,7', 'description' => 'A tripulação da nave Covenant descobre um planeta aparentemente inexplorado, mas logo se depara com horrores indescritíveis. Enquanto lutam por sobrevivência, segredos sobre a origem dos xenomorfos vêm à tona em mais um capítulo sombrio da icônica franquia Alien.'],
+  
+  ['id' => 7, 'title' => 'Divertidamente 2', 'gender' => 'Animação', 'year' => 2024, 'img' => 'divertidamente.png', 'stars' => '4,7', 'description' => 'Agora adolescente, Riley enfrenta novas emoções enquanto cresce e passa por mudanças. Alegria, Tristeza, Raiva, Medo e Nojinho precisam aprender a lidar com Ansiedade e outras novidades que chegam à mente da jovem. Uma continuação sensível e divertida da jornada emocional.'],
+  
+  ['id' => 8, 'title' => 'Mad Max: Estrada da Fúria', 'gender' => 'Ação', 'year' => 2015, 'img' => 'mad-max.png', 'stars' => '4,7', 'description' => 'Em um mundo pós-apocalíptico, Max se une à Imperatriz Furiosa em uma fuga desesperada contra um tirano brutal. Em alta velocidade pelo deserto, eles desafiam as regras do caos em uma obra visualmente insana, repleta de adrenalina e rebelião.'],
+];
